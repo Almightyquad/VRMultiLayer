89b847f5bb36d2286c792d906211bc3ff8ae771b
Marius Stubberud <marius.stubberud.92@gmail.com>
2017-10-06 13:51:44 +0200

    did some work on the terrain, now have a miniature version of the map. The textures aren't there yet, but the heightmap is.

---
+++ Assets/Minimap.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour {
+    TerrainData terrainData;
+    int hmW;
+    int hmH;
+    float[,] heights;
+    // Use this for initialization
+    void Awake () {
+        TerrainData terrainDataCopy = GameObject.Find("Terrain").GetComponent<Terrain>().terrainData;
+        hmW = terrainDataCopy.heightmapWidth;
+        hmH = terrainDataCopy.heightmapHeight;
+        heights = terrainDataCopy.GetHeights(0, 0, hmW, hmH);
+        
+
+    }
+
+    void Start()
+    {
+        Debug.Log("I AM RUNNING");
+        //this.transform.GetComponent<Terrain>().terrainData = terrainData;
+        this.transform.GetComponent<Terrain>().terrainData.SetHeights(0, 0, heights);
+        this.transform.GetComponent<Terrain>().drawTreesAndFoliage = false;
+        this.transform.GetComponent<Terrain>().terrainData.size = new Vector3(1f,1f,1f);
+        //this.transform.GetComponent<Terrain>().terrainData.size.Set(1f, 1f, 1f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
